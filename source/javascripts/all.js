e727de9831e98e0548503f76cc21235356cf8507
Chris Porter <general@madebyporter.com>
2018-11-06 20:01:54 -0500

    disabled js and added bottom pages

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,59 +1,38 @@
-// import uuid from 'uuid/v4';
-//
-// const $button = document.querySelector('button');
-//
-// const handler = StripeCheckout.configure({
-//   key: STRIPE_PUBLISHABLE_KEY,
-//   image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
-//   locale: 'auto',
-//   closed: function () {
-//
-//   },
-//   token: function(token) {
-//
-//     fetch(`${LAMBDA_ENDPOINT}purchase`, {
-//       method: 'POST',
-//       body: JSON.stringify({
-//         token,
-//         amount,
-//         idempotency_key: uuid()
-//       }),
-//       headers: new Headers({
-//         'Content-Type': 'application/json'
-//       })
-//     })
-//     .then(res => res.json())
-//     .catch(error => console.error(error))
-//     .then(response => {
-//
-//
-//     });
-//   }
-// });
-//
-// $button.addEventListener('click', () => {
-//   // setTimeout(() => {
-//   //   $button.innerHTML = 'Waiting for response...';
-//   // }, 500);
-//
-//   if ($button.id = "loops"){
-//     handler.open({
-//       amount: 500,
-//       name: 'Loops',
-//       description: 'Buy Loops'
-//     });
-//   } else if ($button.id = "tracks"){
-//     handler.open({
-//       amount: 2000,
-//       name: 'Tracks',
-//       description: 'Tracks'
-//     });
-//   } else if ($button.id = "stems"){
-//     handler.open({
-//       amount: 5000,
-//       name: 'Stems',
-//       description: 'Stems'
-//     });
-//   }
-//
-// });
+var js = js || {},
+  body = document.getElementsByTagName('body')[0];
+
+// Scripts
+js.main = {
+  init: function () {
+    // this.modal();
+  },
+  modal: function (e) {
+    var bd = body;
+    var modal_link = document.querySelectorAll('.modal-link');
+    var modal_close = document.querySelectorAll('.modal-close');
+    var modal_page = document.querySelectorAll('.modal-page');
+
+
+    function modal_init(e) {
+      e.preventDefault();
+
+      for(var i = 0, l=document.links.length; i<l; i++) {
+        var $href = this.modal_link.getAttribute('href');
+      }
+
+      document.body.classList.add("modal-open");
+      document.getElementByID($href).classList.add("active");
+    };
+    function modal_remove() {
+      document.body.classList.remove("modal-open");
+      document.modal_page.classList.remove("active");
+    };
+
+    modal_link[0].addEventListener("click", modal_init, false);
+    modal_close[0].addEventListener("click", modal_remove, false);
+  }
+};
+
+document.addEventListener('DOMContentLoaded', function(){
+  js.main.init();
+});
