94346c089705c6719c3251852e6eadbe64d2ad7a
madebyporter <general@madebyporter.com>
2019-12-21 16:23:38 -0800

    made gibberish url index

--- config.rb
+++ config.rb
@@ -62,7 +62,7 @@ activate :gibberish do |gibberish|
   # set the default password
   gibberish.password = 'tunas-violate-arose-unpeeled-terse-quinn'
   # encrypt a page with the default password
-  gibberish.encrypt '/vault/madebyporter.html'
+  gibberish.encrypt '/vault/madebyporter/index.html'
 end
 
 # activate :contentful do |f|
