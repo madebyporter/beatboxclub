d9d8b13634f85fd82d4de263678324c8fdabca43
madebyporter <general@madebyporter.com>
2021-05-15 13:57:36 -0700

    added .html back to template in config

--- config.rb
+++ config.rb
@@ -60,7 +60,7 @@ end
 # ignore 'templates/*.html'
 if @app.data.try(:site).try(:beattapes)
   data.site.beattapes.each do |id, tapes|
-    proxy "/tapes/#{tapes['tape_id'].downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')}", "/tapes/template", 
+    proxy "/tapes/#{tapes['tape_id'].downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')}", "/tapes/template.html", 
     :locals => { :tapes => tapes, :tape_id => tapes.tape_id }, 
     :ignore => true
   end
