8c0fdbcaef9e55206a85e217af9c1fac2e68db7d
madebyporter <general@madebyporter.com>
2021-05-15 13:50:14 -0700

    made proxy relative in config

--- config.rb
+++ config.rb
@@ -60,7 +60,9 @@ end
 # ignore 'templates/*.html'
 if @app.data.try(:site).try(:beattapes)
   data.site.beattapes.each do |id, tapes|
-    proxy "/tapes/#{tapes['tape_id'].downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')}", "/tapes/template.html", :locals => { :tapes => tapes, :tape_id => tapes.tape_id }, :ignore => true
+    proxy "/tapes/#{tapes[1]['tape_id'].downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')}", "/tapes/template", 
+    :locals => { :tapes => tapes[1], :tape_id => tapes.tape_id }, 
+    :ignore => true
   end
 end
 
