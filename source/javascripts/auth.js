e6935df5f300311df9a12597b9f552aa7ce12977
Michael Podolsky <michaelspodolsky@gmail.com>
2022-07-25 14:05:28 -0700

    hide login/logout button if user is already logged into another producer role

--- source/javascripts/auth.js
+++ source/javascripts/auth.js
@@ -3,20 +3,26 @@ const LOGOUT_TEXT = "Logout Library";
 
 (handleAuth = () => {
   netlifyIdentity.init();
+
   const button = document.querySelector(".user-auth-button");
   if (!button) return;
 
   const initialUser = netlifyIdentity.currentUser();
 
-  let userRole = initialUser ? initialUser.app_metadata.roles?.[0] : "";
+  let userRole = initialUser?.app_metadata?.roles?.[0] ?? "";
 
   const buttonText = button.children[0];
-  buttonText.innerText =
-    initialUser && userRole === currentProducer ? LOGOUT_TEXT : LOGIN_TEXT;
+
+  if (initialUser) {
+    if (userRole === currentProducer) buttonText.innerText = LOGOUT_TEXT;
+    else button.style.display = "none";
+  } else {
+    buttonText.innerText = LOGIN_TEXT;
+    button.style.display = "flex";
+  }
 
   button.addEventListener("click", () => {
     const user = netlifyIdentity.currentUser();
-
     if (user && userRole === currentProducer) {
       netlifyIdentity.logout();
     } else {
@@ -27,9 +33,11 @@ const LOGOUT_TEXT = "Logout Library";
   netlifyIdentity.on("login", (u) => {
     userRole = u.app_metadata.roles?.[0];
     if (userRole === currentProducer) buttonText.innerText = LOGOUT_TEXT;
+    else button.style.display = "none";
     netlifyIdentity.close();
   });
   netlifyIdentity.on("logout", () => {
     buttonText.innerText = LOGIN_TEXT;
+    button.style.display = "flex";
   });
 })();
