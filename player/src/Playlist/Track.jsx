abc24cc4c23b46a8a412e40871485841b5cf7c3e
madebyporter <general@madebyporter.com>
2022-07-30 16:25:28 -0700

    updated playlist sizing, add genre, updated icons

--- player/src/Playlist/Track.jsx
+++ player/src/Playlist/Track.jsx
@@ -30,23 +30,24 @@ const Track = ({
         className="box-player-play box-player-control-activate"
         onClick={onPlay}
       >
-        <i className="fas fa-play" />
+        <i className="fa-light fa-play" />
       </div>
       <div
         className="box-player-pause box-player-control-activate"
         onClick={onPause}
       >
-        <i className="fas fa-pause" />
+        <i className="fa-light fa-pause" />
       </div>
     </div>
 
     <div className="box-player-meta">
       <div className="box-meta-ele box-meta-main">
         <h4 className="box-meta-name">{track.name}</h4>
-        <span className="box-meta-author">by {track.author}</span>
+      </div>
+      <div className="box-meta-ele box-meta-genre">
+        <span className="box-meta-value">{track.genre}</span>
       </div>
       <div className="box-meta-ele box-meta-vibe">
-        <h5 className="box-meta-title">Vibe</h5>
         {track.vibe.map((vibe) => (
           <span className="box-meta-tag box-meta-value" key={vibe}>
             {vibe}
@@ -54,8 +55,7 @@ const Track = ({
         ))}
       </div>
       <div className="box-meta-ele box-meta-bpm">
-        <h5 className="box-meta-title">BPM</h5>
-        <span className="box-meta-bpm box-meta-value">{track.bpm}</span>
+        <span className="box-meta-value">{track.bpm}</span>
       </div>
     </div>
 
